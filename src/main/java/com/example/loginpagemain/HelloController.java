deff90b49b8af7476311e930939df672a986264a
Arnob <arnob32116@gmail.com>
2024-01-22 00:37:38 +0600

    build 0.0.3

--- src/main/java/com/example/loginpagemain/HelloController.java
+++ src/main/java/com/example/loginpagemain/HelloController.java
@@ -4,6 +4,7 @@ import animatefx.animation.Bounce;
 import animatefx.animation.FadeIn;
 import animatefx.animation.FadeInRight;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -13,19 +14,25 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable{
         private Stage stage;
         private Scene scene;
         private Parent root;
+        private List<TextField> textFields = new ArrayList<>();
 
-        @FXML
+
+    @FXML
         private Button signin_button;
         @FXML
         private Hyperlink signup_button,forget_pass_button;
@@ -34,7 +41,7 @@ public class HelloController implements Initializable{
         @FXML
         private Pane pane1,pane2,pane3,pane4;
         @FXML
-        public void button_animation(ActionEvent event) throws Exception{
+        public void signin_button_control(ActionEvent event) throws Exception{
             new FadeInRight(signin_button).play();
             Parent root = FXMLLoader.load(getClass().getResource("SignupPage.fxml"));
             stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
@@ -45,7 +52,7 @@ public class HelloController implements Initializable{
             new FadeInRight(root).play();
         }
         @FXML
-        public void sign_up_method(ActionEvent event) throws Exception{
+        public void sign_up_btn_control_method(ActionEvent event) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("SignupPage.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -55,8 +62,21 @@ public class HelloController implements Initializable{
         }
 
         @FXML
-        void unclick_textarea(ActionEvent event) throws Exception{
-            
+        public void unclick_textarea(MouseEvent event) throws Exception{
+            Pane pane = (Pane) event.getSource();
+            for (Node node : pane.getChildren()) {
+                if (node instanceof TextField) {
+                    textFields.add((TextField) node);
+                }
+            }
+
+            for(TextField text_fields : textFields){
+                if(text_fields.isFocused()){
+                    pane.requestFocus();
+                    break;
+                }
+            }
+
         }
 
 
