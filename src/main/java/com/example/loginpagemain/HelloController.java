aa0bdaf50d7fa82b5bda9c54234c39d19eea674d
Arnob <arnob32116@gmail.com>
2024-01-24 01:28:55 +0600

    build 0.0.4

--- src/main/java/com/example/loginpagemain/HelloController.java
+++ src/main/java/com/example/loginpagemain/HelloController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -24,6 +25,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Stack;
+
 
 public class HelloController implements Initializable{
         private Stage stage;
@@ -31,6 +34,7 @@ public class HelloController implements Initializable{
         private Parent root;
         private List<TextField> textFields = new ArrayList<>();
 
+        private Stack<Pane> panes = new Stack<>();
 
     @FXML
         private Button signin_button;
@@ -39,7 +43,9 @@ public class HelloController implements Initializable{
         @FXML
         private BorderPane borderpane1;
         @FXML
-        private Pane pane1,pane2,pane3,pane4;
+        private Pane pane1,pane2,pane3,pane4,pane5;
+        @FXML
+        private Button next_button , back_button, back_button_page2;
         @FXML
         public void signin_button_control(ActionEvent event) throws Exception{
             new FadeInRight(signin_button).play();
@@ -61,8 +67,41 @@ public class HelloController implements Initializable{
         new FadeInRight(root).play();
         }
 
+
         @FXML
-        public void unclick_textarea(MouseEvent event) throws Exception{
+        public void signup_next(ActionEvent event) throws Exception{
+            Button button =(Button) event.getSource();
+
+            if (button == next_button && panes.size()==0 ){
+                panes.push(pane3);
+                panes.push(pane5);
+            }
+            else if (button == next_button && panes.size()==1){
+                panes.push(pane5);
+            }
+            else if(button==back_button || button==back_button_page2){
+                if (panes.size()!=0)
+                panes.pop();
+               // System.out.println(panes.firstElement());
+            }
+            if(panes.empty()) {
+                Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+                stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+                new FadeInRight(root).play();
+            }
+            else {
+                new FadeInRight(panes.peek()).play();
+                panes.peek().toFront();
+            }
+
+        }
+
+
+        @FXML
+        public void unclick_textarea(MouseEvent event) throws Exception {
             Pane pane = (Pane) event.getSource();
             for (Node node : pane.getChildren()) {
                 if (node instanceof TextField) {
@@ -78,6 +117,24 @@ public class HelloController implements Initializable{
             }
 
         }
+
+        @FXML
+        public void back_button_control(ActionEvent event){
+
+        }
+
+        @FXML
+        public void number_constrains_for_text_fields(KeyEvent event){
+                TextField numfield = (TextField) event.getSource();
+                if(!event.getCharacter().matches("\\d")){
+                    event.consume();
+                    numfield.setStyle("-fx-border-color: red");
+                }
+                else {
+                    numfield.setStyle("-fx-border-color: black");
+                }
+        }
+
 
 
     @Override
